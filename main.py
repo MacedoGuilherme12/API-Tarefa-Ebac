326d58c52d563273d26d2d21b6e2c5706eb70887
Guilherme <guilhermecedoanastacio@gmail.com>
2025-07-22 17:22:03 -0300

    Rotas Post e Put ajustados

--- main.py
+++ main.py
@@ -19,17 +19,30 @@ class Tarefa(BaseModel):
     concluida : Optional[bool] = False
 
 db_tarefas = {}
-@app.get('/livros/{id_tarefa}')
-@app.get("/existe")
-def exibe():
-    return {"Livros" : db_tarefas}
+
+@app.get("/tarefas/{id_tarefa}")
+def getTarefa(id_tarefa: int):
+    if id_tarefa in db_tarefas:
+        return {"data": db_tarefas[id_tarefa]}
+    raise HTTPException(status_code=409,detail="Essa tarefa não existe!")
+        
+
+@app.get("/tarefas")
+def getTarefas():
+    return {"Tarefas" : db_tarefas}
 
 
 @app.post("/adiciona_tarefa")
 def post_tarefa(tarefa : Tarefa):
     for id_tarefasDb in db_tarefas.keys():
-        if db_tarefas[id_tarefasDb]["nome"] == tarefa.nome:
+        if db_tarefas[id_tarefasDb].nome == tarefa.nome:
             raise HTTPException(status_code=409, detail="Tarefa ja existe!")
     id_tarefa = str(uuid.uuid4()) 
-    db_tarefas[id_tarefa] = tarefa.model_dump()
-    return { "msg" : "Tarefa adicionada com sucesso!", "Tarefa" : db_tarefas[id_tarefa]}
+    db_tarefas[id_tarefa] = tarefa
+    return { "message" : "Tarefa adicionada com sucesso!", "id" : id_tarefa, "Tarefa" : db_tarefas[id_tarefa]}
+
+@app.delete('/delete/{nome}')
+def deletar_tarefa(nome : str):
+    for nome in db_tarefas.values():
+        return {"message" : nome}
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
